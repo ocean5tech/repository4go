dc4bc4eacd5937f221664560ea3e0d63829771b5
ocean5apphotmail <ocean5app@hotmail.com>
2023-02-06 23:32:35 +0800

    bookstoreV1

---
+++ go-bookstore/cmd/main/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"hello/go-bookstore/pkg/routes"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/ocean5tech/repository4go/go-bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
--- go-bookstore/pkg/routes/bookstore-routes.go
+++ go-bookstore/pkg/routes/bookstore-routes.go
@@ -1,1 +1,16 @@
 package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/ocean5tech/repository4go/go-bookstore/pkg/controllers"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+
+	router.HandleFunc("/books/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/books/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/books/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/books/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/books/{bookId}", controllers.DeleteBook).Methods("DELETE")
+
+}
