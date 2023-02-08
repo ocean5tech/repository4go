cdf7eae7ff16075fa1267c40b343a2c99f5b4fe3
ocean5apphotmail <ocean5app@hotmail.com>
2023-02-08 09:43:56 +0800

    bookstore

--- go-bookstore/cmd/main/main.go
+++ go-bookstore/cmd/main/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"hello/go-bookstore/pkg/routes"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"github.com/ocean5tech/repository4go/tree/main/go-bookstore/pkg/routes"
+	"github.com/ocean5tech/repository4go/go-bookstore/pkg/routes"
 )
 
 func main() {
