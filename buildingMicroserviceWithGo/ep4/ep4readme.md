339be7ba07883f65cb3a28b580b3612a70a563d1
ocean5apphotmail <ocean5app@hotmail.com>
2023-02-11 23:47:41 +0800

    ep4 done

--- buildingMicroserviceWithGo/ep4/ep4readme.md
+++ buildingMicroserviceWithGo/ep4/ep4readme.md
@@ -1,23 +1,7 @@
 go mod init github.com/ocean5tech/repository4go/buildingMicroserviceWithGo/ep4
 
-d, err := json.Marshal(lp) 用来把对象转换成JSON
+git pull
 
-curl localhost:9090 | jq 可以格式化表示json，但是在windows需要另外设定
+git checkout -b  episode_4
 
-vscode的扩展插件 postcode，有选项可以把测试request转成各种语言的代码
-
-
-tags： 指定json各种item的行为
-
-type Product struct {
-	ID          int     `json:"id"`
-	Name        string  `json:"name"`
-	Description string  `json:"description"`
-	Price       float32 `json:"price"`
-	SKU         string  `json:"sku"`
-	CreatedOn   string  `json:"-"`
-	UpdatedOn   string  `json:"-"`
-	DeletedOn   string  `json:"-"`
-}
-
-encode和Marshal，功能差不多，性能快很多
+curl localhost:9090 -d '{}'  可以send一个json
--- buildingMicroserviceWithGo/ep4/handlers/products.go
+++ buildingMicroserviceWithGo/ep4/handlers/products.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/ocean5tech/repository4go/buildingMicroserviceWithGo/ep4/data"
 )
@@ -21,14 +23,47 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	//handle update
-	// goto ep4
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+
+	}
+
+	if r.Method == http.MethodPut {
+		// expert the id in the uri
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		g := reg.FindAllStringSubmatch(r.URL.Path, -1)
+
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+		}
+
+		if len(g[0]) != 2 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+		}
+
+		idString := g[0][1]
+
+		id, err := strconv.Atoi(idString)
+
+		if err != nil {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+		}
+
+		p.l.Println("got id : ", id)
+		p.updateProducts(id, rw, r)
+
+		return
+
+	}
 	//catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 
 }
 
 func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
+
+	p.l.Println("Handle Get Products")
 	lp := data.GetProducts()
 	// d, err := json.Marshal(lp)   //被ToJSON替换了
 	err := lp.ToJSON(rw)
@@ -36,4 +71,42 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
 	// rw.Write(d)  //ToJSON里面包含了io.Writer，不在需要write
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle Post Products")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	p.l.Printf("Prod: %#v", prod)
+
+	data.AddProduct(prod)
+}
+
+func (p *Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle Put Product")
+
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+		return
+	}
+
+	err = data.UpdateProduct(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusBadRequest)
+		return
+	}
+
+	if err != nil {
+		http.Error(rw, "Error Internal", http.StatusInternalServerError)
+		return
+	}
+
 }
